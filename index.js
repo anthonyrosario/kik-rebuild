1ca228cbd281ee3dd3f63c8dc7cdd9aae2fe6201
Anthony Rosario <rosariojanthony@gmail.com>
2017-02-12 17:43:32 -0800

    created server file with basic routing and configurations

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const favicon = require('serve-favicon');
+
+
+app.set('port', (process.env.PORT || 3000));
+
+// app.use(favicon(__dirname + '/public/images/favicon.ico'));
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+app.get('/', (req, res) => {
+  return res.render('index', {title: 'Home'});
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Server is running on port', app.get('port'));
+});
