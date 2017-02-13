a014512dc2b907a7210b55ea25d8f9ac0d203292
Anthony Rosario <rosariojanthony@gmail.com>
2017-02-12 20:20:44 -0800

    added navbar menu button and made navbar mobile responsive

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,21 @@
 var $appsDownloadBtn = $('.apps-download-btn');
 var $appDownloadLinks = $('#app-download-links');
 
+var $navMenuBtn = $('.navbar-menu-btn');
+var $navMenu = $('#main-nav-links');
+
 $appsDownloadBtn.on('click', function() {
-  console.log();
   if ($appDownloadLinks.css('display') == 'none') {
     $appDownloadLinks.css('display', 'block');
   } else {
     $appDownloadLinks.css('display',  'none');
+  }
+});
+
+$navMenuBtn.on('click', function() {
+  if ($navMenu.css('height') == '0px') {
+    $navMenu.css('height', 'auto');
+  } else {
+    $navMenu.css('height',  '0px');
   }
 });
